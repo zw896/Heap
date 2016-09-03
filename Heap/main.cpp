42ac69eb40abd68c923a3798bf502d38b63ec47e
zw896 <wzz343@gmail.com>
2016-09-03 13:39:10 +1000

    Initial Commit

---
+++ Heap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Heap
+//
+//  Created by zw896 on 3/09/2016.
+//  Copyright © 2016 UOW. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
